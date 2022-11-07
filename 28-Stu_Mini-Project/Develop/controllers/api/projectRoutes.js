f954b0be3afb725430a4e03b3305463e6fa2a41d
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-07 14:05:50 -0800

    create homepage handlebars and route

--- 28-Stu_Mini-Project/Develop/controllers/api/projectRoutes.js
+++ 28-Stu_Mini-Project/Develop/controllers/api/projectRoutes.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
 
+router.get('/', async (req, res) =>{
+  try {
+    const dbProjectData = await Project.findAll({
+      include: {attributes: ['name']},
+    });
+    console.log("dbProjectData", dbProjectData);
+    const projects = dbProjectData.map((project) => project.dataValues);
+    console.log("projects", projects);
+    res.render('hompage', { projects: projects});
+  }catch (err){
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const newProject = await Project.create({
@@ -11,6 +25,20 @@ router.post('/', async (req, res) => {
     res.status(200).json(newProject);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.get('/project/:id', async (req, res)=>{
+  try{
+    const dbProjectData = await Project.findByPk({
+      include: {attributes: ['name', 'description', 'date-created', 'needed_funding']},
+    });
+    console.log("dbProjectData", dbProjectData);
+    const projects = dbProjectData.map((projectDeets) => projectDeets.dataValues);
+    console.log("projects", projects);
+    res.render('hompage', { projects: projects});
+  }catch (err){
+    res.status(500).json(err);
   }
 });
 
